454316188d1142a0df2179d09fc4e51f58e07852
“renatodebritto” <renatodebritto@gmail.com>
2022-08-19 09:30:10 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,32 +1,59 @@
-<?php 
+<?php
+session_start();
+require_once "vendor/autoload.php";
+require_once "functions.php";
 
-require_once("vendor/autoload.php");
+use Hcode\Model\User;
 
-use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
-
-$app = new Slim();
+$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
-$page = new Page();
+$app->get('/', function () {
 
-$page->setTpl("index");
-
-});
-
-$app->get('/admin', function () {
-
-    $page = new PageAdmin();
+    $page = new Hcode\Page();
 
     $page->setTpl("index");
 
 });
 
+$app->get('/admin', function () {
+
+    User::verifyLogin();
+
+    $page = new Hcode\PageAdmin();
+
+    $page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function () {
+
+    $page = new Hcode\PageAdmin([
+        "header" => false,
+        "footer" => false,
+    ]);
+
+    $page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function () {
+
+    User::login(post('deslogin'), post('despassword'));
+
+    header("Location: /admin");
+    exit;
+
+});
+
+$app->get('/admin/logout', function () {
+
+    User::logout();
+
+    header("Location: /admin/login");
+    exit;
+
+});
 
 $app->run();
-
- ?>
